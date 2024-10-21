2e6ba12b519fad29db933a6827f6dfc4683a1b68
denis <G00403126@atu.ie>
2024-10-21 21:57:46 +0100

    aaa

---
+++ src/main/java/ie/atu/week5fixed/Week5FixedApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5fixed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5FixedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5FixedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5fixed/Week5FixedApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5fixed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5FixedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
